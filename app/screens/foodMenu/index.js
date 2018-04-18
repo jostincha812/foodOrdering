b565bd805acfb6f8695578537e71b3c7b56112a2
jostincha812 <jostin.cha812@gmail.com>
2018-04-18 20:08:14 +0530

    Integrated food menu with firebase

--- app/screens/foodMenu/index.js
+++ app/screens/foodMenu/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { RefreshControl } from 'react-native';
+import { RefreshControl, Alert } from 'react-native';
 import { Container, Content, Text } from 'native-base';
+import firebase from 'react-native-firebase';
 
 import NavBar from '../../components/navBar';
 import CustomList from '../../components/customList';
@@ -16,19 +17,41 @@ export default class FoodMenu extends Component {
 		isRefreshing : false,
 		data : []
 	};
+
+	componentDidMount () {
+		this.fetchMenu();
+	}
 		
 	fetchMenu = ( isRefreshing ) => {
 		this.setState( { 
 			loading : isRefreshing ? false : true,
 			isRefreshing : isRefreshing || false
 		 } );
-		 
-		 setTimeout( () => {
-			 this.setState( { 
- 				loading : false,
- 				isRefreshing : false
- 			 } );
-		 }, 500);
+
+		const { navigation : { state : { params : { restaurantId } } } } = this.props;
+		 firebase.firestore().collection(`restaurants/${restaurantId}/foodMenu`).onSnapshot((documentSnapshot) => {
+			let resultData = [];
+			documentSnapshot.forEach((doc) => {
+				let data = doc.data();
+				data.Id = doc.id;
+				resultData.push(data);
+			});
+			console.log('resultData ::::::: ', resultData);
+			this.setState( { 
+				rawData : resultData,
+				data : resultData, 
+				loading : false, 
+				isRefreshing : false
+			 } );
+		}, err => {
+			this.setState( { loading : false } );
+			Alert.alert('Food Menu','Oops.. Something went wrong, Please try again',
+				[
+					{ text : 'OK' }
+				]
+			);
+		});
+
 	}
 	
 	getMenu = ( item ) => {
